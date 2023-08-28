5b392ef920d5e00e82dd12720b850a1d595d5e4b
Chris Nguyen <cnguyen0320@gmail.com>
2023-08-28 07:45:10 -0700

    Create title for login

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		VStack{
+			
+			// header
+			ZStack{
+				RoundedRectangle(cornerRadius:0)
+					.foregroundColor(Color.pink)
+					.rotationEffect(Angle(degrees: 15.0))
+					
+				
+				VStack{
+					Text("ToDoList")
+						.font(.system(size:50))
+						.foregroundColor(Color.white)
+						.bold()
+					
+					Text("Get it done!")
+						.font(.system(size:30))
+						.foregroundColor(Color.black)
+						.bold()
+					
+				}
+				.padding(.top,30)
+			}
+			.offset(y:-100)
+			.frame(width:UIScreen.main.bounds.width * 3, height:300)
+			
+			
+			
+			// login form
+			
+			// create account
+			
+			Spacer()
+		}
     }
 }
 
