18acf925e00ba60af1fb525789ed3eb35392a1bc
Chris Nguyen <cnguyen0320@gmail.com>
2023-08-30 18:39:54 -0700

    Save To Do List item

--- ToDoList/Models/ToDoListItem.swift
+++ ToDoList/Models/ToDoListItem.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+
+struct ToDoListItem: Codable, Identifiable {
+	let id: String
+	let title: String
+	let dueDate: TimeInterval
+	let createdDate: TimeInterval // when item created
+	var isDone: Bool
+	
+	mutating func setDone(_ state: Bool){
+		isDone = state
+	}
+}
--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Chris Nguyen on 8/28/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 
@@ -19,8 +21,32 @@ class NewItemViewViewModel: ObservableObject {
 		
 	}
 	
-	
 	func save(){
+		guard canSave() else{
+			return
+		}
+		
+		// get current user ID
+		guard let uId = Auth.auth().currentUser?.uid else {
+			return
+		}
+		
+		// Create model for the item
+		let newId = UUID().uuidString
+		let newItem = ToDoListItem(id: newId,
+								   title: title,
+								   dueDate: dueDate.timeIntervalSince1970,
+								   createdDate: Date().timeIntervalSince1970,
+								   isDone: false)
+		
+		// Save model
+		let db = Firestore.firestore()
+		db.collection("users")
+			.document(uId)
+			.collection("todos")
+			.document(newId)
+			.setData(newItem.asDictionary())
+		
 		
 	}
 	
